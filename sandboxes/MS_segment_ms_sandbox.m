9c58cebcfd7fa1bbbeadbe74d39e0c9a15aaf572
ecarmichael <ecarmichael>
2019-12-13 17:55:14 -0500

    added in new segragation and append functions as sandboxes.  alignment is close

---
+++ sandboxes/MS_segment_ms_sandbox.m
@@ -0,0 +1,87 @@
+function ms_out = MS_segment_ms_sandbox(ms_in)
+%% MS_segment_ms: segments the previously concatinated data in the ms stucture
+%   MS_segment_ms takes the previously concatenated and processed miniscope
+%   data in the 'ms' data structure from
+%   'msGenerateVideoObj_ConcatenateBatch' in
+%   https://github.com/torilynntemple/MiniscopeAnalysisConcatenation by
+%   Tori-Lynn Temple (Williams lab at McGill) and breaks it back down into
+%   segments based on unique recording periods.  
+%
+%   Inputs:
+%       ms_in: [struct] this is the data structure as output by msGenerateVideoObj_ConcatenateBatch
+%       
+%   Outputs:
+%       ms_out: [struct] same as ms_in but with new segmented fields for
+%       time, RawTraces, FiltTraces, vidNum, frameNum. 
+%
+%
+%   To Do:
+%       - get file name, date, and other identifiers for the video files
+%       - have a feature that checks the camera idea based on the video
+%       file names (behav vs mscope) and check the samples add up to frames
+%       - append LFP data (provided alignment is spot on...
+%       - figure out what is happeneing with the ms.timestamps0 and
+%       ms.timestamps1 being different. missing frame correction? 
+%
+%
+%
+% EC: 2019-12-13:
+%   - initial sandbox with basic segmentation using samples in the
+%   ms.timestamps and ms.time fields
+%% intialize
+
+ms_out = ms_in; 
+
+% remove the fields that will be replaced (for cleanliness atm)
+ms_out = rmfield(ms_out, 'RawTraces');
+ms_out = rmfield(ms_out, 'FiltTraces');
+ms_out = rmfield(ms_out, 'time');
+ms_out = rmfield(ms_out, 'vidNum');
+ms_out = rmfield(ms_out, 'frameNum');
+
+
+
+%% get all the time blocks and restrict the data to those blocks
+
+times = ms_in.timestamps1;
+
+% make incremental 
+all_times = nan(1,length(times));
+for iT = 1:length(times)
+    if iT == 1
+        all_times(iT)  =  times(iT);
+    else
+        all_times(iT)  =  times(iT) + all_times(iT-1);
+    end
+end
+
+% check 
+if all_times(end) ~= length(ms_in.time)
+    error('Time segments and the total number of frames from ms.time do not align')
+end
+    
+% loop though the times and restrict to the individual segments    
+for iT = 1:length(all_times)
+    if iT == 1 
+        ms_out.time{iT,1} = ms_in.time(1:all_times(iT));
+        ms_out.RawTraces{iT,1} = ms_in.RawTraces(1:all_times(iT),:);
+        ms_out.FiltTraces{iT,1} = ms_in.FiltTraces(1:all_times(iT),:);
+        ms_out.frameNum{1,iT} = ms_in.frameNum(1,1:all_times(iT));
+        ms_out.vidNum{1,iT} = ms_in.vidNum(1,1:all_times(iT));
+    else 
+        ms_out.time{iT,1} = ms_in.time(all_times(iT-1)+1:all_times(iT));
+        ms_out.RawTraces{iT,1} = ms_in.RawTraces(all_times(iT-1)+1:all_times(iT),:);
+        ms_out.FiltTraces{iT,1} = ms_in.FiltTraces(all_times(iT-1)+1:all_times(iT),:);
+        ms_out.frameNum{1,iT} = ms_in.frameNum(1,all_times(iT-1)+1:all_times(iT));
+        ms_out.vidNum{1,iT} = ms_in.vidNum(1,all_times(iT-1)+1:all_times(iT));
+    end
+    if length(ms_out.time{iT,1}) ~= times(iT)
+        error('Time segments are not aligning propertly.')
+    end
+end
+
+%% clean up
+
+ms_out.format = 'Segmented based on timestamp periods only'; 
+
+
