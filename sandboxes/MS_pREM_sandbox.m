2a3e03d7a3dff3d3d083cf488d533103e44e26e4
Eric Carmichael <ecarmichael@gmail.com>
2021-06-12 13:37:56 -0400

    added phasic REM script.  initial IPI done but needs crit 1-3

--- sandboxes/MS_pREM_sandbox.m
+++ sandboxes/MS_pREM_sandbox.m
@@ -26,16 +26,70 @@
 %     3. epoch theta amp > mean theta amp for all REM. 
 
 
-
+LFP_dir = 'J:\Williams_Lab\Jisoo\LFP data\Jisoo'; 
 addpath(genpath('C:\Users\ecarm\Documents\GitHub\CEH2'));
 addpath(genpath('C:\Users\ecarm\Documents\GitHub\vandermeerlab\code-matlab\shared'));
 cd('C:\Users\ecarm\Dropbox (Williams Lab)\JisooProject2020\2020_Results_aftercutting\Across_episodes\Inter\PV1069\10_22_2019_PV1069_HATSwitch')
 
 
 c_ord = linspecer(5); % just some nicer colours. 
+
 %% load some pre-cut data
 
 load('ms_resize.mat');
+
+inter_dir = cd; % where to write back the sleep scored data.  
+%% get the session name
+parts = strsplit(cd,  filesep);
+
+session = parts{end};
+subject = parts{end-1};
+date = parts{end}(1:10); 
+type = strsplit(parts{end}, '_');
+type = type{end}; 
+if strcmp(type,'HATSwitch') && contains(subject, 'PV1069')
+    type = 'HATD6_switch'; 
+end
+
+%%  manually score some data
+
+% get the LFP files
+cd(LFP_dir)
+
+this_LFP_dir = MS_list_dir_names(cd, {subject, type}); 
+
+cd(this_LFP_dir{1});
+
+% get the channels to load from the pre-cut data. First channel should be
+% EMG and second is the best LFP. 
+cfg_load = [];
+for iC = 1:length(ms_seg_resize.NLX_csc{1}.cfg.hdr) % loop over channels. 
+    cfg_load.fc{iC} = [ms_seg_resize.NLX_csc{1}.cfg.hdr{iC}.AcqEntName '.ncs'];
+    cfg_load.desired_sampling_frequency = ms_seg_resize.NLX_csc{1}.cfg.hdr{iC}.SamplingFrequency; 
+end
+
+% load some data. 
+CSC = MS_LoadCSC(cfg_load); 
+
+
+%% score the sleep data
+ emg_h = abs(hilbert(CSC.data(1,:))); % get the emg power for plotting.        
+ 
+ cfg_sleep = [];
+ cfg_sleep.tvec_range = [0 5];  % number of seconds per window.
+ cfg_sleep.emg_range = [min(emg_h) mean(emg_h) + std(emg_h)*5]; % default, should be based on real data.
+ cfg_sleep.emg_chan = 1; % emg channel.  Can be empty.
+ cfg_sleep.lfp_chans = 1; % lfp channels to be plotted can be empty. Can be 1 or more, but best to keep it less than 3. should be rows in csc.data.
+ cfg_sleep.state_name = {'Wake',       'SWS',       'REM',    'Quiescence','Transition','pREM',  'Redo',     'Exit'}; %
+ cfg_sleep.state_keys = {'rightarrow','uparrow', 'downarrow', 'leftarrow', 'numpad0',   'numpad1' 'backspace','backquote' }; % which key to press for each state_val
+ 
+
+ score = MS_Sleep_score_UI(cfg_sleep, CSC.tvec,CSC.data(2,:), emg_h);
+
+
+
+%% write the hypno back to the intermediate dir.  
+
 
 
 %% grab the REM blocks and filter them with the Mizuseki 2011 config. 
@@ -87,7 +141,15 @@ IPI{iR} = diff(theta_csc{iR}.tvec(peaks));
 
 % smooth with 11 sample rec window.  warning, gives much lower IPIs. Use
 % for distribution only. 
-IPI_smooth{iR} = conv2(IPI{iR},gausswin(11), 'same'); 
+IPI_smooth{iR} = conv2(IPI{iR},rectwin(11), 'same'); 
+
+
+
+% [TODO] fill in IPI values per cycle to match the actual data. 
+
+
+
+
 
 
 % make a sample plot if needed
@@ -114,6 +176,20 @@ IPI_smooth{iR} = conv2(IPI{iR},gausswin(11), 'same');
 % histogram(IPI_smooth,25)
 % legend('IPI', 'IPI smoothed')
 
+% figure(102)
+% ax(1) = subplot(2,2,1:2);
+% hold on
+% plot(theta_csc{iR}.tvec, theta_csc{iR}.data,'color',  c_ord(1,:))
+% plot(theta_csc{iR}.tvec, amp{iR}, 'color', c_ord(2,:))
+% ax(2) = subplot(2,2,3:4);
+% hold on
+% plot(theta_csc{iR}.tvec(peaks(1:end-1)), IPI_smooth{iR}, 'color',  c_ord(4,:))
+% plot(theta_csc{iR}.tvec(peaks(1:end-1)), IPI{iR}, 'color', c_ord(1,:))
+% linkaxes(ax, 'x')
+% 
+% pause
+% close all
+
 end
 %%  collect the IPIs to get a distribution
 all_IPI = []; all_IPI_smooth = []; % collect the ISI for crit 1
@@ -127,21 +203,21 @@ for iR = REM_idx
     all_amp = [all_amp, amp{iR}];
 end
 
-L10_prctile = prctile(1./all_IPI_smooth, 10); 
-L5_prctile = prctile(1./all_IPI_smooth, 5); 
-L50_prctile = prctile(1./all_IPI_smooth, 50);
+L10_prctile = prctile(all_IPI_smooth, 10); 
+L5_prctile = prctile(all_IPI_smooth, 5); 
+L50_prctile = prctile(all_IPI_smooth, 50);
 
-figure(102)
-subplot(1,2,1)
-histogram(1./all_IPI,50, 'facecolor', c_ord(1,:));
-xlabel('Freq Hz')
+
+subplot(3,2,5)
+histogram(all_IPI,50, 'facecolor', c_ord(1,:));
+xlabel('IPI')
 legend('IPI')
 
-subplot(1,2,2)
-histogram(1./all_IPI_smooth,50,'facecolor', c_ord(4,:));
+subplot(3,2,6)
+histogram(all_IPI_smooth,50,'facecolor', c_ord(4,:));
 vline([L10_prctile,L5_prctile, L50_prctile], {'k', 'r', 'm'}, {'L10', 'L5', 'L50'}); 
 legend('IPI smoothed')
-xlabel('Freq Hz')
+xlabel('IPI')
 
 
 %% Crit 1 find blocks with >900ms duration. 
