6d6b33d054243bfcb3d28c56c33c6b48514f0e84
ecarmichael@gmail.com <ecarmichael@gmail.com>
2021-07-02 16:32:16 -0400

    started dSub sandbox

---
+++ sandboxes/dSub_NLX_screener_sandbox.m
@@ -0,0 +1,19 @@
+%% dSub NLX screening sandbox
+
+
+% this is a simple sandbox for loading, and checking the spatial tuning
+% properties of cells using NLX
+
+
+%% add some code to the path
+
+addpath(genpath('/home/williamslab/Documents/Github/vandermeerlab/code-matlab/shared'));
+
+addpath(genpath('/home/williamslab/Documents/Github/CEH2')); 
+
+data_dir = '/home/williamslab/Dropbox (Williams Lab)/Williams Lab Team Folder/Eric/dSubiculum/inProcess/M23_2021-07-02_OF';
+
+
+cd(data_dir); % go to the data folder specified above
+%% load dada
+
