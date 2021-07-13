7ee07d449599d8cbac769a96738480409c2d2422
ecarmichael@gmail.com <ecarmichael@gmail.com>
2021-07-13 15:16:57 -0400

    fixed % in sleep score gui

--- Common_functions/MS_Sleep_score_UI.m
+++ Common_functions/MS_Sleep_score_UI.m
@@ -283,7 +283,7 @@ while ishandle(h)
         fprintf('%s ', cfg.state_name{iK})
         score(this_idx(1):this_idx(2)) = cfg.state_val(iK);
         x_lim = xlim;
-        fprintf('     %d   %d    %0.2d%%', x_lim(1), x_lim(2), round(x_lim(2)/tvec(end),2))
+        fprintf('     %.0f   %.0f    %0.2f%%\n', x_lim(1), x_lim(2), round(x_lim(2)/(tvec(end)-tvec(1)),2)*100)
         if done == 1 
             disp('Exiting')
             break
