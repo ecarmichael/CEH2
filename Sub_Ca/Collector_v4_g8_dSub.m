f63c75f83a2e4661cc1a540eb3d40798bd12d24b
ecarmichael@gmail.com <ecarmichael@gmail.com>
2023-01-25 14:40:49 -0500

    started collect for dsub_g8

---
+++ Sub_Ca/Collector_v4_g8_dSub.m
@@ -0,0 +1,23 @@
+function Collector_v4_g8_dSub(data_dir, inter_dir)
+%% Collector_v4_g8_dSub: Preprocess and collect v4 data for the dSub g8 W_maze and OF data. 
+%  - data structs:
+%       - HD from 'head
+%
+%
+%    Inputs: 
+%    - data_dir: [path]  path to data folders
+%
+%    - inter_dir: [path] where you want the intermediate files to be saved.
+%    
+%
+%    Outputs: 
+%    -
+%
+%
+%
+%
+% EC 2023-01-25   initial version 
+%
+%
+%
+%% initialize
