e7c6e7b4ef9fbe7973a1e0da3e69b084131b83fa
ecarmichael@gmail.com <ecarmichael@gmail.com>
2023-06-20 19:23:16 -0400

    trap/bp sandbox, WIP shell and ms concat

---
+++ SH/cnmfe_single.sh
@@ -0,0 +1,6 @@
+cp /home/ecar/Github/MATLAB_1pCalAnalysis/MiniscopeAnalysis-master/msRunSingleFolder_EC.m .
+module load StdEnv/2020
+module load gentoo/2020
+module load matlab/2020a
+
+matlab -nodisplay -batch 'msRunSingleFolder_EC'
