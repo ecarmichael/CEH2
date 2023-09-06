f2d5d041d697e538141089f6092fa38970fb9001
Eric Carmichael <ecarmichael@gmail.com>
2023-09-06 07:27:32 -0400

    Assembly REM

--- sandboxes/sandbox_PCA_ICA.m
+++ sandboxes/sandbox_PCA_ICA.m
@@ -1,13 +1,22 @@
 %% sandbox_PCA/ICA
 
-codebase_dir = 'C:\Users\ecarm\Documents\GitHub\'; 
+codebase_dir = 'C:\Users\ecarm\Documents\GitHub\vandermeerlab\code-matlab\shared'; 
+ca_dir = 'C:\Users\ecarm\Documents\GitHub\CEH2'; 
+
+
 code_dir = 'C:\Users\ecarm\Downloads\Dos-Santos Assembly ICA\Dos-Santos Assembly ICA';
 
 RnR_dir = 'C:\Users\ecarm\Documents\GitHub\RnR_methods'; 
 
 % data_dir = 'C:\Users\ecarm\Williams Lab Dropbox\Williams Lab Team Folder\Eric\Maze_Ca\inter\pv1254\2021_12_17_pv1254_MZD3' %C:\Users\ecarm\Dropbox (Williams Lab)\Williams Lab Team Folder\Eric\Maze_Ca\inter\pv1254\2021_12_17_pv1254_MZD3'; 
-data_dir = 'C:\Users\ecarm\Williams Lab Dropbox\Eric Carmichael\Comp_Can_inter'
+data_dir = 'C:\Users\ecarm\Williams Lab Dropbox\Eric Carmichael\Comp_Can_inter'; 
+rem_dir = 'C:\Users\ecarm\Williams Lab Dropbox\Eric Carmichael\JisooProject2020\2020_Results_aftercutting\Across_episodes\Inter'; 
 restoredefaultpath
+
+cd('C:\Users\ecarm\Documents\GitHub\OASIS_matlab')
+oasis_setup
+
+addpath(genpath(ca_dir)); 
 addpath(genpath(codebase_dir))
 addpath(genpath(RnR_dir));
 
@@ -15,19 +24,26 @@ addpath(code_dir)
 
 cd(data_dir)
 
+rng(123, 'twister')
+
 %% 
 
 % load('ms_trk.mat')
 % load('behav_DLC.mat')
+this_sess = 'pv1069_LTD5_data.mat'; 
 
-load('pv1069_LTD5_data.mat')
+load(this_sess)
 
-behav = MS_align_data(behav,ms)
+behav = MS_align_data(behav,ms); 
+
+move_idx = behav.speed > 2.5; 
+%%
 
 ms_trk = ms; 
 keep_idx = zeros(1,size(ms_trk.RawTraces,2));
 keep_idx(1:floor(size(ms_trk.RawTraces,2)*.66)) = 1; 
 
+remove_cell_id = find(~keep_idx); 
 
 cfg_rem = [];
 cfg_rem.remove_idx = find(~keep_idx);
@@ -41,6 +57,8 @@ ms_trk_rem = MS_append_deconv(ms_trk_rem);
 
 keep_idx = sum(ms_trk_rem.deconv, 1) >0; 
 
+remove_cell_id_decon = find(~keep_idx); 
+
 cfg_rem = [];
 cfg_rem.remove_idx = find(~keep_idx);
 cfg_rem.data_type = 'deconv'; 
@@ -49,15 +67,14 @@ ms_trk_rem = MS_Remove_trace(cfg_rem, ms_trk_rem);
 % deconv the all_b
 
 
-
 %% follow grosmark et al. method of deconv preprocessing 
 Csp = ms_trk_rem.deconv./ms_trk_rem.denoise; 
 Csp = Csp > 0.01; 
 ms_trk_rem.Csp = Csp; 
 
-cfg_plot.Ca_type = 'RawTraces'; 
-cfg_plot.plot_type = '2d'; 
-MS_plot_ca(cfg_plot, ms_trk_rem)
+% cfg_plot.Ca_type = 'RawTraces'; 
+% cfg_plot.plot_type = '2d'; 
+% MS_plot_ca(cfg_plot, ms_trk_rem)
 
 %% bin and convolve
 binsize = 0.1; % in seconds, so everything else should be seconds too
@@ -68,17 +85,17 @@ gau_sdf = conv2(Csp,gk,'same'); % convolve with gaussian window
 
 gau_z = zscore(gau_sdf, [], 2); 
 %% plot the gaussian smoothed Csp
-figure(101)
-ax(1)= subplot(5,1,1);
-hold on
-plot(ms_trk.time, behav.position(:,1)); 
-plot(ms_trk.time, behav.position(:,2)); 
-xlim([ms_trk.time(1) ms_trk.time(end)])
-
-ax(2) = subplot(5,1,2:5);
-imagesc(ms_trk.time, 1:size(Csp,2), gau_z')
-
-linkaxes(ax, 'x')
+% figure(101)
+% ax(1)= subplot(5,1,1);
+% hold on
+% plot(ms_trk.time, behav.position(:,1)); 
+% plot(ms_trk.time, behav.position(:,2)); 
+% xlim([ms_trk.time(1) ms_trk.time(end)])
+% 
+% ax(2) = subplot(5,1,2:5);
+% imagesc(ms_trk.time, 1:size(Csp,2), gau_z')
+% 
+% linkaxes(ax, 'x')
 
 
 % %% optional bin the time into something larger than a frame. 
@@ -108,7 +125,7 @@ tbin_centers = tbin_edges(1:end-1)+binsize/2; % vector of time bin centers (for 
 data_h = [];
 for ii = size(Csp,2):-1:1
 
-this_cell = ms.time(find(Csp(:,ii)))/1000;
+this_cell = ms.time(find(Csp(: ,ii) & move_idx))/1000; 
 
 spk_count = histc(this_cell,tbin_edges); % get spike counts for each bin
 spk_count = spk_count(1:end-1); % ignore spikes falling exactly on edge of last bin.
@@ -122,11 +139,11 @@ tvec = tbin_centers;
 
 
 %% convert data if needed. 
-gau_z_t = gau_z;
-gau_sdf_t = gau_sdf;
-
-gau_z = data_int; 
-gau_sdf = data_int; 
+% gau_z_t = gau_z;
+% gau_sdf_t = gau_sdf;
+% 
+% gau_z = data_int; 
+% gau_sdf = data_int; 
 
 %% try the assembly code....
 
@@ -150,27 +167,39 @@ for ii = size(Ass_Temp,2):-1:1
 end
 
 %% keep assemblies with strong activations
-keep_idx = zeros(1,size(Ass_Temp,2));  
+keep_idx = zeros(1,size(Ass_Temp,2)); 
+Ass_pos_cells = cell(size(keep_idx)); 
 for ii = size(Ass_Temp,2):-1:1
-
+    
    if max(Ass_Temp(:, ii)) > 0.2
        keep_idx(ii) = 1; 
+       
+       z_weight = zscore(Ass_Temp(:,ii)); 
+
+       Ass_pos_cells{ii} = find(z_weight > 2); 
+
    end
-    
+   
+   
 end
 
 Ass_pos = Ass_Temp; 
 Ass_pos(:,~keep_idx) = []; 
+Ass_pos_cells(~keep_idx) = []; 
 
 time_proj_pos = time_proj;
 time_proj_pos(~keep_idx,:) = [];
 %% stem plot for first few ensembles 
- figure(303); hold on
- c_ord = parula(8);
-for ii = 1:8
-    subplot(4,2,ii)
+ figure(303);clf; hold on
+ c_ord = parula(size(Ass_pos,2)+2);
+for ii = 1:size(Ass_pos,2)
+    subplot(4, ceil(size(Ass_pos,2)/4),ii)
+    hold on
         stem(Ass_pos(:,ii), 'color', c_ord(ii,:))
         view(90,90)
+        
+        stem(Ass_pos_cells{ii}, Ass_pos(Ass_pos_cells{ii},ii), 'color', c_ord(ii,:), 'MarkerFaceColor', c_ord(ii,:))
+        
 end
 
 %% plot the output
@@ -178,7 +207,7 @@ end
 figure(202)
 clf
 % maximize
-ax(1) = subplot(6, 1, 1)
+ax(1) = subplot(6, 1, 1);
 hold on
 plot(ms_trk.time/1000, behav.position(:,1)); 
 % plot(ms_trk.time/1000, behav.position(:,2)); 
@@ -248,7 +277,7 @@ m = 2;
 for ii = 1: size(time_proj_pos,1)
     subplot(m,n,ii)
     hold on
-    [~, p_idx] = findpeaks(zscore(time_proj_pos(ii,:)),'MinPeakHeight', 1.96   ,'MinPeakDistance', 2*floor(mode(diff(ms.time)))); 
+    [~, p_idx] = findpeaks(zscore(time_proj_pos(ii,:)),'MinPeakHeight', 1.96 ,'MinPeakDistance', 2*floor(mode(diff(ms.time)))); 
     
     this_pos = [];
     for ip = 1:length(p_idx)
@@ -273,54 +302,122 @@ for ii = 1: size(time_proj_pos,1)
         end
 end
 
-%% final figure with coloured assemblies for # 3 and 9. 
-Ass_idx = [3 9]; 
 
-figure(202)
-clf
-% maximize
-ax(1) = subplot(6, 1, 1)
+%% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% grab the REM data
+sess = strsplit(this_sess, '_'); 
+sub = sess{1}; 
+sess = sess{2}; 
+cd(rem_dir )
+
+sub_list = dir('PV*');
+keep_idx = [];
+for ii = 1:length(sub_list)
+    if strfind(lower(sub_list(ii).name), lower(sub))
+        keep_idx(ii) = 1; 
+    else
+        keep_idx(ii) = 0;
+    end
+end
+
+cd([rem_dir filesep sub_list(find(keep_idx)).name filesep])
+    
+sess_list = dir('*T*');
+keep_idx = [];
+for ii = 1:length(sess_list)
+    if strfind(lower(sess_list(ii).name), lower(sess))
+        keep_idx(ii) = 1; 
+    else
+        keep_idx(ii) = 0;
+    end
+end
+
+cd(sess_list(find(keep_idx)).name)
+
+load('all_detrendRaw_post_REM.mat')
+
+%remove cells that were excluded in the awake set. 
+
+all_detrendRaw_post_REM(:, remove_cell_id) = [];
+
+all_detrendRaw_post_REM(:, remove_cell_id_decon) = []; 
+
+%% deconvolve
+
+  fprintf('\n<strong>%s</strong>: deconvolving traces...\n', mfilename)
+    for iChan = size(all_detrendRaw_post_REM,2):-1:1
+            tic;
+            [denoise,deconv] = deconvolveCa(all_detrendRaw_post_REM(:,iChan), 'foopsi', 'ar2', 'smin', -2.5, 'optimize_pars', true, 'optimize_b', true);
+            toc;
+            all_denoise(:,iChan) = denoise;    all_deconv(:,iChan) = deconv;
+    end
+
+    
+% follow grosmark et al. method of deconv preprocessing 
+Csp_rem = all_deconv./all_denoise; 
+Csp_rem = Csp_rem > 0.01; 
+
+
+%% bin and convolve
+binsize = 0.1; % in seconds, so everything else should be seconds too
+gauss_window = 1./binsize; % 1 second window
+gauss_SD = 0.5./binsize; % 0.02 seconds (20ms) SD
+gk = gausskernel(gauss_window,gauss_SD); gk = gk./binsize; % normalize by binsize
+gau_sdf_rem = conv2(Csp_rem,gk,'same'); % convolve with gaussian window
+
+gau_z_rem = zscore(gau_sdf_rem, [], 2); 
+
+%%
+rem_time =  0:1/mode(diff(ms.time)):(length(all_deconv)/mode(diff(ms.time))); 
+rem_time = rem_time(1:end-1); 
+
+binsize = .5;
+tbin_edges_rem = 0:binsize:(length(all_deconv)/mode(diff(ms.time))); % vector of time bin edges (for histogram)
+tbin_centers_rem = tbin_edges_rem(1:end-1)+binsize/2; % vector of time bin centers (for plotting)
+ 
+data_h_rem = [];
+for ii = size(Csp_rem,2):-1:1
+
+this_cell = rem_time(find(Csp_rem(:,ii)));
+
+spk_count = histc(this_cell,tbin_edges_rem); % get spike counts for each bin
+spk_count = spk_count(1:end-1); % ignore spikes falling exactly on edge of last bin.
+
+data_h_rem(:,ii) = spk_count; 
+end
+
+
+
+tvec_rem = tbin_centers_rem; 
+
+%% try the assembly code 
+
+
+Ass_Temp_rem = assembly_patterns(data_h_rem');
+
+%using wake assemblies. 
+wake_time_proj_rem = assembly_activity(Ass_pos,data_h_rem'); 
+
+%% plot the wake Assemblies in REM
+
+figure(5); clf; 
+ax(1) = subplot(3,1,1:2);
+imagesc(tvec_rem, 1:size(all_detrendRaw_post_REM,2), zscore(all_detrendRaw_post_REM,0, 1))
+caxis([0 10])
+
+
+ax(2)= subplot(3,1,3);
 hold on
-plot(ms_trk.time/1000, behav.position(:,1)); 
-% plot(ms_trk.time/1000, behav.position(:,2)); 
-xlim([ms_trk.time(1)/1000 ms_trk.time(end)/1000])
-ylabel('position on track (cm)')
-set(gca, 'XTick', []);
+c_ord = cool(size(wake_time_proj_rem,1)+4); 
 
-ax(2) = subplot(6,1,2:4);
-imagesc(ms.time/1000, 1:size(gau_z,2), gau_z')
-% hold on
-% for ii = size(gau_sdf, 2):-1:1
-%     s_t = ms_trk.time(Csp(:,ii) >0)/1000;
-%     if ~isempty(s_t)
-%        plot([s_t, s_t]', [(ones(size(s_t))*ii)-.5, (ones(size(s_t))*ii)+.5]', 'color', 'k', 'linewidth', 4)
-%         
-%     end
-% %     plot(ms_trk.time, gau_sdf(:,ii)+ii)
-% end
-xlim([ms_trk.time(1)/1000 ms_trk.time(end)/1000])
-ylim([0 size(Csp, 2)])
-set(gca, 'XTick', [], 'YDir', 'normal') 
-ylabel('cell ID')
+for ii = 1:size(wake_time_proj_rem, 1)
 
-c = colorbar('Location', 'east'); 
-c.AxisLocation = 'out'; 
-pos = c.Position; 
-c.Position = [pos(1)+.025 pos(2) pos(3) pos(4)]; 
-c.Label.String = 'z-score activity'; 
-c.Label.FontSize = 20;
-
-ax(3) = subplot(6,1,5:6);
-cla
-hold on
-c_ord = cool(size(time_proj_pos,1)+4); 
-for ii = 1:size(time_proj_pos,1)
 %    nan_idx = time_proj_pos(ii,:) < prctile(time_proj_pos(ii,:), 10);
 %    plot3(ms_trk.time(~nan_idx)/1000, zscore(time_proj_pos(ii,~nan_idx)),ones(1, sum(~nan_idx))*100* ii, '.', 'color', c_ord(ii,:)) 
 %        plot3(tvec, zscore(time_proj_pos(ii,:)),ones(1, length(time_proj_pos(ii,:)))*100* ii, 'color', c_ord(ii,:)) 
-       plot(tvec, zscore(time_proj_pos(ii,:))+ii*10, 'color', c_ord(ii+4,:)) 
+       plot(tvec_rem, zscore(wake_time_proj_rem(ii,:))+ii*10, 'color', c_ord(ii+4,:)) 
 
-       tick_val(ii) = mode(zscore(time_proj_pos(ii,:))+ii*10); 
+       tick_val(ii) = mode(zscore(wake_time_proj_rem(ii,:))+ii*10); 
        tick_label{ii} = num2str(ii); 
 end
 % view(0,  15)
@@ -328,8 +425,8 @@ set(gca, 'YTick', tick_val, 'YTickLabel', tick_label)
 
 ylabel('assembly ID')
 xlabel('time (s)')
-ylim([min(zscore(time_proj_pos(1,:))+10), max(zscore(time_proj_pos(1,:))+10*size(time_proj_pos,1))])
-xlim([tvec(1) tvec(end)])
+ylim([min(zscore(wake_time_proj_rem(1,:))+10), max(zscore(wake_time_proj_rem(1,:))+10*size(wake_time_proj_rem,1))])
+xlim([tvec_rem(1) tvec_rem(end)])
 %     xlim([ms_trk.time(1)/1000 ms_trk.time(end)/1000])
 %     zlim([0 size(time_proj_pos,1)*100])
 %     set(gca, 'color', 'k')
