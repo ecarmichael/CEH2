75b67a3dac13d22a6f3b3df49136a24bf2babe84
ecarmichael@gmail.com <ecarmichael@gmail.com>
2023-09-18 16:44:43 -0400

    OE binary loaders

---
+++ Common_functions/OE_plot_binary.m
@@ -0,0 +1,27 @@
+function OE_plot_binary(stream, channels)
+%% OE_plot_binary:
+%
+%
+%
+%    Inputs: 
+%    -
+%
+%
+%
+%    Outputs: 
+%    -
+%
+%
+%
+%
+% EC 2023-09-18   initial version 
+%
+%
+%
+%% initialize
+
+if nargin <1
+%     channels = length(stream.sm);
+end
+
+display('1')
