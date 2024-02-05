95e4dd7498316eb21d5dfda6c2e8e2c4f8fda6ad
Eric Carmichael <ecarmichael@gmail.com>
2024-02-05 09:09:14 -0500

    asmbly plot change

--- Common_functions/Pipline_Asmbly_plot.m
+++ Common_functions/Pipline_Asmbly_plot.m
@@ -1,4 +1,4 @@
-function Pipline_Asmbly_plot(A_out, fig_dir); 
+function Pipline_Asmbly_plot(A_out, fig_dir);
 
 
 %% initialize
@@ -18,14 +18,14 @@ end
 
 for iB = size(A_out,2):-1:1
     MS_Asmbly_plot(A_out{iB}.P_temp, A_out{iB}.P_pos, A_out{iB}.map, A_out{iB}.P_loc, fig_dir, A_out{iB}.info);
-close all;
+    close all;
 end
 
-%% summarize the reactivations 
+%% summarize the reactivations
 
 for iB = size(A_out,2):-1:1
     MS_Asmbly_ReAct_plot(A_out{iB}, fig_dir);
-
+    
 end
 
 
@@ -37,7 +37,7 @@ for iB = size(A_out,2):-1:1
     elseif (isempty(A_out{iB}.P_pos) || ~isfield(A_out{iB}, 'P_pos'))
         continue
     else
-        p_idx = 1:length(A_out{iB}.P_pos); 
+        p_idx = 1:length(A_out{iB}.P_pos);
     end
     
     MS_Asmbly_plot_raster(A_out{iB}, fig_dir,p_idx)
@@ -49,17 +49,17 @@ end
 %% plot the raw/raster for the sleep periods
 for iB = size(A_out,2):-1:1
     
-        if length(A_out{iB}.P_pos) >3
+    if length(A_out{iB}.P_pos) >3
         p_idx = 1:4;
     elseif (isempty(A_out{iB}.P_pos) || ~isfield(A_out{iB}, 'P_pos'))
         continue
     else
-        p_idx = 1:length(A_out{iB}); 
+        p_idx = 1:length(A_out{iB});
     end
     
     MS_Asmbly_plot_raster_ReAct(A_out{iB},fig_dir,'REM_Pre_data',p_idx)
     
     
     MS_Asmbly_plot_raster_ReAct(A_out{iB},fig_dir,'REM_Post_data',p_idx)
-
+    
 end
