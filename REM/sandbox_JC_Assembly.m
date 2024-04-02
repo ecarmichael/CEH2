ed407a31af367799f095b24fac164c388aab9d47
Eric Carmichael <ecarmichael@gmail.com>
2024-04-01 23:17:03 -0400

    added lfp to JC cc migration

--- REM/sandbox_JC_Assembly.m
+++ REM/sandbox_JC_Assembly.m
@@ -188,3 +188,135 @@ for iF = length(f_list):-1:1
     
     clear all_* ms behav info
 end
+
+
+%% get the trimmed LFP data
+
+% collect the raw and detrend files;
+inter_dir = 'C:\Users\ecarm\Williams Lab Dropbox\Eric Carmichael\Comp_Can_inter';
+data_dir = 'C:\Users\ecarm\Williams Lab Dropbox\Eric Carmichael\JisooProject2020\2020_Results_aftercutting\Across_episodes\Inter';
+cd(data_dir)
+sub_list = dir('pv*');
+
+for iSub = length(sub_list):-1:1
+    cd([data_dir filesep sub_list(iSub).name]);
+    
+    
+    for iSub = length(sub_list):-1:1
+        cd([data_dir filesep sub_list(iSub).name]);
+        
+        
+        LT_list = dir('*LTD*');
+        HAT_list= dir('*HATD*');
+        sess_list=[LT_list; HAT_list];
+        
+        
+        for iS  = length(sess_list):-1:1
+            if strcmpi(sess_list(iS).name(end), '3') 
+                continue
+            end
+            
+            cd([data_dir filesep sub_list(iSub).name filesep sess_list(iS).name]);
+            
+            if ~exist('ms_resize.mat', 'file')
+                continue
+            end
+                
+            warning off
+            load('ms_resize.mat')
+            
+            % grab all the trimmed LFP data;
+            
+            sws_lfp_pre = []; sws_emg_pre = []; sws_tvec_pre = []; sws_ttls_pre =[];
+            sws_lfp_post = []; sws_emg_post = []; sws_tvec_post = []; sws_ttls_post =[];
+            rem_lfp_pre = []; rem_emg_pre = []; rem_tvec_pre = []; rem_ttls_pre = [];
+            rem_lfp_post = []; rem_emg_post = []; rem_tvec_post = []; rem_ttls_post = [];
+
+            for iR = 1:length(ms_seg_resize.NLX_csc)
+                
+                if strcmp(ms_seg_resize.hypno_label{iR}, 'SW') && strcmp(ms_seg_resize.pre_post{iR}, 'pre')
+                    sws_lfp_pre = [sws_lfp_pre, ms_seg_resize.NLX_csc{iR}.data(2,:)];     
+                    sws_emg_pre = [sws_emg_pre, ms_seg_resize.NLX_csc{iR}.data(1,:)];
+                    sws_tvec_pre = [sws_tvec_pre, ms_seg_resize.NLX_csc{iR}.tvec'];   
+                    sws_ttls_pre = [sws_ttls_pre, ms_seg_resize.NLX_evt{iR}.t{end}]; 
+                    
+                elseif strcmp(ms_seg_resize.hypno_label{iR}, 'SW') && strcmp(ms_seg_resize.pre_post{iR}, 'post')
+                    sws_lfp_post = [sws_lfp_post, ms_seg_resize.NLX_csc{iR}.data(2,:)];     
+                    sws_emg_post = [sws_emg_post, ms_seg_resize.NLX_csc{iR}.data(1,:)];
+                    sws_tvec_post = [sws_tvec_post, ms_seg_resize.NLX_csc{iR}.tvec'];   
+                    sws_ttls_post = [sws_ttls_post, ms_seg_resize.NLX_evt{iR}.t{end}]; 
+
+
+                elseif strcmp(ms_seg_resize.hypno_label{iR}, 'REM') && strcmp(ms_seg_resize.pre_post{iR}, 'pre')
+                    rem_lfp_pre = [rem_lfp_pre, ms_seg_resize.NLX_csc{iR}.data(2,:)];
+                    rem_emg_pre = [rem_emg_pre, ms_seg_resize.NLX_csc{iR}.data(1,:)];
+                    rem_tvec_pre = [rem_tvec_pre, ms_seg_resize.NLX_csc{iR}.tvec'];
+                    rem_ttls_pre = [rem_ttls_pre, ms_seg_resize.NLX_evt{iR}.t{end}];
+                    
+                elseif strcmp(ms_seg_resize.hypno_label{iR}, 'REM') && strcmp(ms_seg_resize.pre_post{iR}, 'post')
+                    rem_lfp_post = [rem_lfp_post, ms_seg_resize.NLX_csc{iR}.data(2,:)];     
+                    rem_emg_post = [rem_emg_post, ms_seg_resize.NLX_csc{iR}.data(1,:)];
+                    rem_tvec_post = [rem_tvec_post, ms_seg_resize.NLX_csc{iR}.tvec'];   
+                    rem_ttls_post = [rem_ttls_post, ms_seg_resize.NLX_evt{iR}.t{end}]; 
+
+                end
+
+            end
+
+            pre_rem = [rem_tvec_pre; rem_lfp_pre; rem_emg_pre]; 
+            post_rem = [rem_tvec_post; rem_lfp_post; rem_emg_post];
+
+            pre_sws = [sws_tvec_pre; sws_lfp_pre; sws_emg_pre];
+            post_sws = [sws_tvec_post; sws_lfp_post; sws_emg_post]; 
+
+            
+                        info = [];
+                        info.subject = sub_list(iSub).name;
+                        info.session = sess_list(iS).name;
+                        info.lfp = ms_seg_resize.NLX_csc{1}.cfg.hdr{1}; 
+                        info.session  = strsplit(info.session, '_'); 
+                        info.session = info.session{end}; 
+            
+            
+            this_file = strsplit(sess_list(iS).name, 'pv');
+            
+            
+                       
+  
+    fname = [inter_dir filesep 'LFP' filesep 'lfp_' strrep(info.subject, 'PV', 'pv') '_'  info.session '.h5'];
+    
+    if exist(fname, 'file')
+        delete(fname)
+    end
+    
+    hdf5write(fname, '/mouse', string(strrep(info.subject, 'PV', 'pv')));
+    hdf5write(fname, '/condition', string(info.session),'WriteMode', 'append');
+    hdf5write(fname, '/chan_ids', {'tvec', 'lfp', 'emg'},'WriteMode', 'append');
+
+        
+    hdf5write(fname, '/pre_rem_lfp', double(pre_rem),'WriteMode', 'append');
+    hdf5write(fname, '/post_rem_lfp', double(post_rem),'WriteMode', 'append');
+    
+    hdf5write(fname, '/pre_sws_lfp', double(pre_sws),'WriteMode', 'append');
+    hdf5write(fname, '/post_sws_lfp', double(post_sws),'WriteMode', 'append');
+    
+    hdf5write(fname, '/pre_rem_ttl', double(rem_ttls_pre),'WriteMode', 'append');
+    hdf5write(fname, '/post_rem_ttl', double(rem_ttls_post),'WriteMode', 'append');
+    
+    hdf5write(fname, '/pre_sws_ttl', double(sws_ttls_pre),'WriteMode', 'append');
+    hdf5write(fname, '/post_sws_ttl', double(sws_ttls_post),'WriteMode', 'append');
+
+
+ 
+  
+            
+            fprintf('Saving %s   %s ...\n', info.subject, info.session)
+%             save([inter_dir filesep info.subject '_' info.session '_data.mat'], 'ms', 'behav',...
+%                 'all_deconv_pre_REM', 'all_deconv_post_REM',...
+%                 'all_denoise_pre_REM', 'all_denoise_post_REM',...
+%                 'all_binary_pre_REM', 'all_binary_post_REM',...
+%                 'all_seg_idx', 'info')
+%             warning on
+        end
+    end
+end
