e52030371b3445a94ec869daeb5111b48958cda6
Eric Carmichael <ecarmichael@gmail.com>
2024-04-29 01:14:53 -0400

    adde movement thresholding and rate maps to CErad ca only

--- CE/CE_Process_Rad_Ca_only.m
+++ CE/CE_Process_Rad_Ca_only.m
@@ -42,10 +42,12 @@ behav_rec_a = MS_align_data(behav_rec, ms_Rec);
 
 %% convert to tsd for restricting data
 
-pos_Enc = tsd(behav_enc_a.time, behav_enc_a.position');  
+pos_Enc = tsd(behav_enc_a.time, behav_enc_a.position');
+pos_Enc.label = {'x', 'y'}; 
 pos_Enc.cfg.hdr{1} = behav_enc_a.json; 
 
 pos_Rec = tsd(behav_rec_a.time, behav_rec_a.position');
+pos_Rec.label = {'x', 'y'}; 
 pos_Rec.cfg.hdr{1} = behav_rec_a.json; 
 
 ms_Enc = tsd(ms_Enc.time, ms_Enc.Binary');
@@ -192,28 +194,110 @@ xlim([bin_Rec_iti.tvec(1) bin_Rec_iti.tvec(end)])
 figure(199)
 clf
 
-c_idx = 110:120; 
+lin_spd = getLinSpd([], pos_Enc_trl); 
 
+cfg = []; cfg.method = 'raw'; cfg.operation = '>'; cfg.threshold = 2.5; % speed limit in cm/sec
+iv_fast = TSDtoIV(cfg,lin_spd); % only keep intervals with speed above thresh
+
+cfg = []; cfg.method = 'raw'; cfg.operation = '<'; cfg.threshold = 35; % speed limit in cm/sec
+iv_max = TSDtoIV(cfg,lin_spd); % only keep intervals with speed above thresh
+
+pos_enc_trl_mov = restrict(pos_Enc_trl, iv_fast);
+pos_enc_trl_mov = restrict(pos_Enc_trl, iv_max);
+
+bin_enc_trl_mov = restrict(bin_Enc_trl, iv_fast);
+bin_enc_trl_mov = restrict(bin_Enc_trl, iv_max);
+
+
+for kk = 1:19:size(bin_enc_trl_mov.data, 1)
+c_idx = kk:kk+19;  
+
+figure(kk)
 for ii = 1:length(c_idx)
-    subplot(4,4,ii)
+    subplot(4,5,ii)
     
-    plot(pos_Enc_trl.data(1,:), pos_Enc_trl.data(2,:), '.k')
+    plot(pos_enc_trl_mov.data(1,:), pos_enc_trl_mov.data(2,:), '.k')
     hold
-    keep_idx = nearest_idx3(bin_Enc_trl.tvec(logical(bin_Enc_trl.data(c_idx(ii),:))), pos_Enc_trl.tvec); 
-    plot(pos_Enc_trl.data(1,keep_idx), pos_Enc_trl.data(2,keep_idx), '.r')
+    keep_idx = nearest_idx3(bin_enc_trl_mov.tvec(logical(bin_enc_trl_mov.data(c_idx(ii),:))), pos_enc_trl_mov.tvec); 
+    s = scatter(pos_enc_trl_mov.data(1,keep_idx), pos_enc_trl_mov.data(2,keep_idx), 20,pos_enc_trl_mov.tvec(keep_idx));
+    s.MarkerFaceColor = s.MarkerEdgeColor; 
     title(num2str(c_idx(ii)))
     
 
 end
 
-
-
-for ii = 1:length(c_idx)
-    
-    
-    
-    
 end
 
+
+%% rate maps
+% set up bins
+SET_xmin = floor(min(pos_enc_trl_mov.data(2,:))/100)*100 ; SET_ymin = floor(min(pos_enc_trl_mov.data(1,:))/100)*100; % set up bins
+SET_xmax = ceil(max(pos_enc_trl_mov.data(2,:))/100)*100; SET_ymax = ceil(max(pos_enc_trl_mov.data(1,:))/100)*100;
+
+% SET_xmin = 100; 
+% SET_xmax = 400;
+% SET_ymin = 200;
+% SET_ymax = 700;
+SET_xBinSz =5; SET_yBinSz = 5;
+
+
+ 
+x_edges = SET_xmin:SET_xBinSz:SET_xmax;
+y_edges = SET_ymin:SET_yBinSz:SET_ymax;
+
+% gaussian kernal
+kernel = gausskernel([1 1],1); % 2d gaussian in bins
+
+% compute occupancy encode
+occ_hist = histcn(pos_enc_trl_mov.data(1:2,:)',y_edges,x_edges); % 2-D version of histc()
+occ_hist = conv2(occ_hist,kernel,'same');
+ 
+no_occ_idx = find(occ_hist < 15); % NaN out bins never visited
+occ_hist(no_occ_idx) = NaN;
+ 
+occ_hist = occ_hist .* (mode(diff(pos_enc_trl_mov.tvec))); % convert samples to seconds using video frame rate (30 Hz)
+
+
+
+m = 4;
+n = 4;
+s1_idx = 1:2:n*m; 
+s2_idx = 2:2:n*m; 
+figure(102)
+clf
+ip = 0; 
+for ii = 1:size(bin_enc_trl_mov.data,1)
+%     keep_idx = nearest_idx3(bin_enc_trl_mov.tvec(logical(bin_enc_trl_mov.data(c_idx(ii),:))), pos_enc_trl_mov.tvec); 
+
+    spk_x = interp1(pos_enc_trl_mov.tvec,pos_enc_trl_mov.data(1,:),bin_enc_trl_mov.tvec(logical(bin_enc_trl_mov.data((ii),:))),'linear');
+    spk_y = interp1(pos_enc_trl_mov.tvec,pos_enc_trl_mov.data(2,:),bin_enc_trl_mov.tvec(logical(bin_enc_trl_mov.data((ii),:))),'linear');
+    if ip >= (n*m)/2
+        figure(102+ii)
+        ip = 1;
+    else
+        ip = ip+1;
+    end
+    
+    disp(ip)
+    subplot(m, n, s1_idx(ip))
+    plot(pos_enc_trl_mov.data(1,:), pos_enc_trl_mov.data(2,:), '.k');
+    title(num2str(ii))
+    hold on
+    plot(spk_x, spk_y, '.r');axis off;
+    
+    % compute the rate map
+    if isempty(spk_x) || isempty(spk_y)
+        continue
+    end
+    spk_hist = histcn([spk_x, spk_y],y_edges,x_edges);
+    spk_hist = conv2(spk_hist,kernel, 'same');
+    
+    spk_hist(no_occ_idx) = NaN;
+    tc = spk_hist./occ_hist;
+
+    subplot(m, n, s2_idx(ip))
+    pcolor(tc'); shading flat; axis off; %colorbar('Location', 'northoutside')
+   
+end
 
 %% save for later. 
