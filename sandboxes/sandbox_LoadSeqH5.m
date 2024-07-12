1955f598f5e034170d5f389ab4f9c2bb6d4e4ec8
ecarmichael <ecarmichael@gmail.com>
2024-07-12 15:25:49 -0400

    minor change to loadSeqH5 for mac

--- sandboxes/sandbox_LoadSeqH5.m
+++ sandboxes/sandbox_LoadSeqH5.m
@@ -2,19 +2,19 @@
 
 
 
-cd('C:\Users\ecarm\Williams Lab Dropbox\Williams Lab Team Folder\Eric\Assembly_EV\output_REM\seqNMF')
+% cd('C:\Users\ecarm\Williams Lab Dropbox\Williams Lab Team Folder\Eric\Assembly_EV\output_REM\seqNMF')
 
 
 f_list = dir('*.h5'); 
 
 
-for iF = 1:length(f_list)
+% for iF = 1:length(f_list)
     
-   this_h = MS_h5_to_stuct('seqReplayResults_LTD1_535_wake_wake.h5')
-    
-    
+   this_h = MS_h5_to_stuct('seqReplayResults_LTD1_540_wake_wake.h5')
     
     
     
     
-end
+    
+    
+% end
