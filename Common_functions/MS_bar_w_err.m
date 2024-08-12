93d16c08c8c4b0dd90ecc27d2ba47b2f99c7c5cc
Eric Carmichael <ecarmichael@gmail.com>
2024-08-12 15:03:07 -0400

    Master As JC breakout

--- Common_functions/MS_bar_w_err.m
+++ Common_functions/MS_bar_w_err.m
@@ -1,7 +1,46 @@
-function [hb, eb] =  MS_bar_w_err(data_a, data_b, color, xval)
+function [hb, eb] =  MS_bar_w_err(data_a, data_b, color, data_flag, stats)
+
+if nargin < 4
+    data_flag = 0; 
+    stats = []; 
+elseif nargin < 5
+    stats = []; 
+end
 
 hb = bar([nanmean(data_a), nanmean(data_b)], 'FaceColor', color(1,:), 'EdgeColor', color(1,:));
 hold on
+
+
+if data_flag && length(data_a) == length(data_b)
+    for ii = length(data_a):-1:1
+        plot(1, data_a(ii), '.', 'color', [.5 .5 .5], 'markersize', 15)
+        plot(2, data_b(ii), '.', 'color', [.5 .5 .5], 'markersize', 15)
+        plot([1 2], [data_a(ii) data_b(ii)], '-', 'color', [.5 .5 .5], 'linewidth', .5)
+    end
+end
+
 eb = errorbar([nanmean(data_a), nanmean(data_b)], [MS_SEM(data_a) ,MS_SEM(data_b)]);
 eb.LineStyle = 'none';
 eb.Color = 'k';
+eb.LineWidth =1.5; 
+
+if ~isempty(stats)
+    switch stats
+        case 'ttest'
+            disp('using ttest')
+            [h, p] = ttest(data_a, data_b);
+        case 'ttest2'
+            disp('using ttest2')
+            [h, p] = ttest2(data_a, data_b);
+            
+        case 'ranksum'
+            disp('using ranksum')
+            [h, p] = ranksum(data_a, data_b);
+    end
+    
+    if h
+        plot(1.5, max([data_a, data_b], [], 'all')*1.15, '*')
+        plot(1:2, [max([data_a, data_b], [], 'all')*1.1 max([data_a, data_b], [], 'all')*1.1], '-k', 'linewidth', 1.5)
+    end
+end
+
