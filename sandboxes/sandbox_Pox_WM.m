51fa2ed338040f49aa36a335e21258962370cffa
Eric Carmichael <ecarmichael@gmail.com>
2024-08-13 15:20:31 -0400

    added pixel converstion to WM

--- sandboxes/sandbox_Pox_WM.m
+++ sandboxes/sandbox_Pox_WM.m
@@ -1,8 +1,8 @@
 % % sandbox_WM_split
 
 if ispc
-cd('C:\Users\ecarm\Williams Lab Dropbox\Williams Lab Team Folder\Eric\PoxR1\WM');
-
+    cd('C:\Users\ecarm\Williams Lab Dropbox\Williams Lab Team Folder\Eric\PoxR1\WM');
+    
 elseif ismac
     cd('/Users/ecar/Williams Lab Dropbox/Williams Lab Team Folder/Eric/PoxR1/WM');
 end
@@ -16,7 +16,7 @@ M_idx = find(contains(POX_tbl.ID, 'Pox'));
 Days = POX_tbl.Properties.VariableNames(2:end-2);
 
 S_loc = {'N', 'E', 'S', 'W'};
-P_loc = {'NE', 'SE', 'SW', 'NW'}; 
+P_loc = {'NE', 'SE', 'SW', 'NW'};
 Fs = 30;
 
 
@@ -40,8 +40,8 @@ for ii = length(M_idx):-1:1
             D_ID{ii}(iD) = 999; % day
         else
             
-        S_dir{ii}(iD) = find(contains(S_loc,Days{iD}(end))); % Start location index
-        D_ID{ii}(iD) = str2double(Days{iD}(end-1)); % day
+            S_dir{ii}(iD) = find(contains(S_loc,Days{iD}(end))); % Start location index
+            D_ID{ii}(iD) = str2double(Days{iD}(end-1)); % day
         end
     end
 end
@@ -50,32 +50,38 @@ end
 
 %% loop over sessions and get the corresponding position.
 
-all_data = []; 
+all_data = [];
 
 % maze specs
-maze.cent = [958.3148 565.6295]; 
-maze.rad = 423.4; 
-maze.xv = maze.rad*1.1*cos(0:pi/100:2*pi)+maze.cent(1); 
-maze.yv = maze.rad*1.1*sin(0:pi/100:2*pi)+maze.cent(2); 
+maze.cent = [958.3148 565.6295];
+maze.rad = 423.4;
+maze.xv = maze.rad*1.1*cos(0:pi/100:2*pi)+maze.cent(1);
+maze.yv = maze.rad*1.1*sin(0:pi/100:2*pi)+maze.cent(2);
+maze.platform_diameter = 12; 
+maze.platform_cent = [1134.3 367.68];
+maze.units = 'cm';
+maze.diameter_cm = 120; 
+maze.diameter_px = 844; 
+maze.conv_fact = maze.diameter_px/maze.diameter_cm; 
 
 c_ord = MS_linspecer(4);
 lgd_names = [];
 
-P_len = []; 
-c_time = []; 
+P_len = [];
+c_time = [];
 TQ = [];
 RQ = [];
 LQ = [];
-OQ = []; 
+OQ = [];
 
-sub = 1:length(M_ID); 
-sess = 1:4; 
+sub = 1:length(M_ID);
+sess = 1:4;
 
 figure(101)
 clf
-for ii = 1:length(sub); 
-
-    t_day = 1:length(D_ID{sub(ii)}); 
+for ii = 1:length(sub);
+    
+    t_day = 1:length(D_ID{sub(ii)});
     for iD = 1:length(t_day)
         %
         
@@ -87,26 +93,19 @@ for ii = 1:length(sub);
         
         if isempty(fname); continue; end
         
-        pos = MS_SLEAP2TSD(fname.name, 30);
+        pos = MS_SLEAP2TSD(fname.name, 30, [], [maze.conv_fact maze.conv_fact]);
         
         pos_r = restrict(pos, pos.tvec(tstart{sub(ii)}(t_day(iD))),  pos.tvec(tend{sub(ii)}(t_day(iD))));
         
-%         % fill in missing values outside of the maze. 
-%         d_tbl = reshape(1:size(pos_r.data,1), 2, size(pos_r.data,1)/2);
-%         for iF = 1:length(pos_r.label)-2
-%             
-%             [isin, ison] = inpolygon(pos_r.data(d_tbl(iF),:), pos_r.data(d_tbl(iF+1),:), maze.xv, maze.yv);
-%             pos_r.data(d_tbl(iF),~(isin|ison)) = NaN; 
-%             pos_r.data(d_tbl(iF+1),~(isin|ison)) = NaN; 
-%             
-%             pos_r.data(d_tbl(iF),:) = fillmissing(pos_r.data(d_tbl(iF)), 'spline', 'EndValues','nearest');
-%             pos_r.data(d_tbl(iF+1),:) = fillmissing(pos_r.data(d_tbl(iF+1)), 'spline', 'EndValues','nearest');
-% 
-%         end
+        % center the data to the middle of the pool;
+        for ii = 1:2:size(pos_r.data,1)-2
+            pos_r.data(ii,:) = pos_r.data(ii,:) - maze.cent(1)/maze.conv_fact; 
+            pos_r.data(ii+1,:) = pos_r.data(ii+1,:) - maze.cent(2)/maze.conv_fact; 
+        end
         
         %%
-                figure(191)
-
+        figure(191)
+        
         subplot(2, length(sub), 1:length(sub));
         hold on
         if ii ==1 && S_dir{sub(ii)}(t_day(iD)) == 1
@@ -135,34 +134,34 @@ for ii = 1:length(sub);
         
         %% compute simple measures
         
-        P_len{sub(ii)}(t_day(iD)) = nansum(sqrt(diff(pos_r.data(1,:)).^2 + diff(pos_r.data(2,:)).^2)); 
-        c_time{sub(ii)}(t_day(iD)) = pos_r.tvec(end) - pos_r.tvec(1); 
+        P_len{sub(ii)}(t_day(iD)) = nansum(sqrt(diff(pos_r.data(1,:)).^2 + diff(pos_r.data(2,:)).^2));
+        c_time{sub(ii)}(t_day(iD)) = pos_r.tvec(end) - pos_r.tvec(1);
         
-        % divide the maze into quadrants. 
+        % divide the maze into quadrants.
         
         QT_idx =(nanmean(pos_r.data([1 3 5],:)) > maze.cent(1)) & (nanmean(pos_r.data([2 4 6],:)) < maze.cent(2));
         QO_idx =(nanmean(pos_r.data([1 3 5],:)) < maze.cent(1)) & (nanmean(pos_r.data([2 4 6],:)) > maze.cent(2));
         QR_idx =(nanmean(pos_r.data([1 3 5],:)) < maze.cent(1)) & (nanmean(pos_r.data([2 4 6],:)) < maze.cent(2));
         QL_idx =(nanmean(pos_r.data([1 3 5],:)) > maze.cent(1)) & (nanmean(pos_r.data([2 4 6],:)) > maze.cent(2));
         
-        % % time in each 
-        QT{sub(ii)}(t_day(iD)) = sum(QT_idx/length(QT_idx))*100; 
+        % % time in each
+        QT{sub(ii)}(t_day(iD)) = sum(QT_idx/length(QT_idx))*100;
         QO{sub(ii)}(t_day(iD)) = sum(QO_idx/length(QO_idx))*100;
-        QR{sub(ii)}(t_day(iD)) = sum(QR_idx/length(QR_idx))*100; 
-        QL{sub(ii)}(t_day(iD)) = sum(QL_idx/length(QL_idx))*100; 
-
-
+        QR{sub(ii)}(t_day(iD)) = sum(QR_idx/length(QR_idx))*100;
+        QL{sub(ii)}(t_day(iD)) = sum(QL_idx/length(QL_idx))*100;
+        
+        
         figure(191)
         hold on
-        plot(nanmean(pos_r.data([1 3 5],QT_idx)), nanmean(pos_r.data([2  4 6], QT_idx)), '.r'); 
-        plot(nanmean(pos_r.data([1 3 5],QO_idx)), nanmean(pos_r.data([2  4 6], QO_idx)), '.b'); 
-        plot(nanmean(pos_r.data([1 3 5],QR_idx)), nanmean(pos_r.data([2  4 6], QR_idx)), '.g'); 
-        plot(nanmean(pos_r.data([1 3 5],QL_idx)), nanmean(pos_r.data([2  4 6], QL_idx)), '.y'); 
-
-        % save the data for later.  
-        all_data.(M_ID{sub(ii)}).(Days{iD}).maze = maze; 
+        plot(nanmean(pos_r.data([1 3 5],QT_idx)), nanmean(pos_r.data([2  4 6], QT_idx)), '.r');
+        plot(nanmean(pos_r.data([1 3 5],QO_idx)), nanmean(pos_r.data([2  4 6], QO_idx)), '.b');
+        plot(nanmean(pos_r.data([1 3 5],QR_idx)), nanmean(pos_r.data([2  4 6], QR_idx)), '.g');
+        plot(nanmean(pos_r.data([1 3 5],QL_idx)), nanmean(pos_r.data([2  4 6], QL_idx)), '.y');
+        
+        % save the data for later.
+        all_data.(M_ID{sub(ii)}).(Days{iD}).maze = maze;
         all_data.(M_ID{sub(ii)}).(Days{iD}).pos_r = pos_r;
-
+        
         
     end
 end
@@ -173,12 +172,12 @@ end
 
 %% PRobe only
 n = ceil(length(M_ID)/2)+1;
-m = 2; 
+m = 2;
 
 
 for ii = 1:length(M_ID)
     for iD = length(D_ID{ii})
-
+        
         
         fname = dir(['labels*Probe*' M_ID{ii}(end-2:end) '*']);
         
@@ -194,27 +193,27 @@ for ii = 1:length(M_ID)
         QR_idx =(nanmean(pos_r.data([1 3 5],:)) < maze.cent(1)) & (nanmean(pos_r.data([2 4 6],:)) < maze.cent(2));
         QL_idx =(nanmean(pos_r.data([1 3 5],:)) > maze.cent(1)) & (nanmean(pos_r.data([2 4 6],:)) > maze.cent(2));
         
-        % % time in each 
-        QT{ii}(iD) = sum(QT_idx/length(QT_idx))*100; 
+        % % time in each
+        QT{ii}(iD) = sum(QT_idx/length(QT_idx))*100;
         QO{ii}(iD) = sum(QO_idx/length(QO_idx))*100;
-        QR{ii}(iD) = sum(QR_idx/length(QR_idx))*100; 
-        QL{ii}(iD) = sum(QL_idx/length(QL_idx))*100; 
+        QR{ii}(iD) = sum(QR_idx/length(QR_idx))*100;
+        QL{ii}(iD) = sum(QL_idx/length(QL_idx))*100;
         
         
         subplot(m, n, ii)
         hold on
         set(gca, 'ydir', 'reverse')
         
-        imagesc(pos.mean_frame); 
+        imagesc(pos.mean_frame);
         colormap('parula')
         scatter(nanmean(pos_r.data([1, 3, 5],:)), nanmean(pos_r.data([2, 4, 6],:)),5, 1:length(pos_r.tvec), '.')
         title({strrep(M_ID{ii}, '_', ' ') ; ['T: ' num2str(QT{ii}(iD),3) ' |O: ' num2str(QO{ii}(iD),3) ' |O: ' num2str(QR{ii}(iD),3) ' |L: ' num2str(QL{ii}(iD),3)]})
         axis square
-       
-        P_QT(ii) = QT{ii}(iD); 
-        P_QO(ii) = QO{ii}(iD); 
-        P_QR(ii) = QR{ii}(iD); 
-        P_QL(ii) = QL{ii}(iD); 
+        
+        P_QT(ii) = QT{ii}(iD);
+        P_QO(ii) = QO{ii}(iD);
+        P_QR(ii) = QR{ii}(iD);
+        P_QL(ii) = QL{ii}(iD);
     end
 end
 
@@ -223,60 +222,88 @@ end
 %% Convert to 'watermaze.csv' for PATHFINDER
 subs = fieldnames(all_data);
 
-target = {'Nose','Head'}; 
+target = {'Nose','Head'};
 
 
 for iS =  1:length(subs)
     %%
-    days= fieldnames(all_data.(subs{iS})); 
-    tbl = table();  
-
+    
+%     figure(iS)
+%     clf
+    days= fieldnames(all_data.(subs{iS}));
+    tbl = table();
+    
     % get the max number of data points for the table
-    max_d = []; 
+    max_d = [];
     for iD = length(days):-1:1
-        max_d(iD) = length(all_data.(subs{iS}).(days{iD}).pos_r.tvec); 
+        max_d(iD) = length(all_data.(subs{iS}).(days{iD}).pos_r.tvec);
     end
-
+    d_ord = MS_linspecer(length(days));
+    
     for iD = 1:length(days)
-
-        d_idx = strfind(all_data.(subs{iS}).(days{iD}).pos_r.cfg.fname{1}, '2024'); 
+        
+        d_idx = strfind(all_data.(subs{iS}).(days{iD}).pos_r.cfg.fname{1}, '2024');
         this_date = all_data.(subs{iS}).(days{iD}).pos_r.cfg.fname{1}(d_idx:d_idx+9);
-        this_date = datetime(this_date, 'InputFormat', 'yyyy_MM_dd', 'Format', 'yyyy/MM/dd'); 
-
-        this_x = fillmissing(mean(all_data.(subs{iS}).(days{iD}).pos_r.data([1, 3, 5 ],:))', 'spline','EndValues','extrap');
-        this_y = fillmissing(mean(all_data.(subs{iS}).(days{iD}).pos_r.data([2, 4, 6],:))', 'spline','EndValues','extrap');
+        this_date = datetime(this_date, 'InputFormat', 'yyyy_MM_dd', 'Format', 'yyyy/MM/dd');
+        
+        %         this_x = fillmissing(mean(all_data.(subs{iS}).(days{iD}).pos_r.data([1, 3, 5 ],:))', 'spline');
+        %         this_y = fillmissing(mean(all_data.(subs{iS}).(days{iD}).pos_r.data([2, 4, 6],:))', 'spline');
+        this_x = mean(all_data.(subs{iS}).(days{iD}).pos_r.data([1, 3, 5 ],:))' - all_data.(subs{iS}).(days{iD}).maze.cent(1);
+        this_y = mean(all_data.(subs{iS}).(days{iD}).pos_r.data([2, 4, 6],:))'- all_data.(subs{iS}).(days{iD}).maze.cent(2);
         this_t = all_data.(subs{iS}).(days{iD}).pos_r.tvec';
+        
+        % find the first non-nan and remove the data up to the first data
+        % point.
+        
+        dat_idx = find(~isnan(this_x));
+        
+        if dat_idx(1) >1
+            this_x(1:dat_idx(1)) = [];
+            this_y(1:dat_idx(1)) = [];
+            this_t(1:dat_idx(1)) = [];
+        end
+        
+        this_t = this_t - this_t(1);
+        %         Flip the Y so that the platform is in the top right quadrant.
+        t_y = this_y;
+        t_y(this_y>0) = -this_y(this_y>0);
+        t_y(this_y<0) = abs(this_y(this_y<0));
+        
+        this_y = t_y;
         
         if length(this_x) <= max(max_d)
             this_x(length(this_x)+1:max(max_d)) = NaN;
             this_y(length(this_y)+1:max(max_d)) = NaN;
             this_t(length(this_t)+1:max(max_d)) = NaN;
         end
-
         
-        tbl = addvars(tbl, [days{iD}; 'x'; num2cell(this_x)]); 
-        tbl = addvars(tbl, [char(this_date); 'y'; num2cell(this_y)]); 
-        tbl = addvars(tbl, [' '; 't'; num2cell(this_t)]); 
+        
+        tbl = addvars(tbl, [days{iD}; 'x'; num2cell(this_x)]);
+        tbl = addvars(tbl, [char(this_date); 'y'; num2cell(this_y)]);
+        tbl = addvars(tbl, [' '; 't'; num2cell(this_t)]);
+        
+        hold on
+        plot(this_x, this_y, '.', 'color', d_ord(iD,:));
     end
-
+    
     % try replacing NaNs by converting to a cell arraty
-    c_tbl = table2cell(tbl); 
-
+    c_tbl = table2cell(tbl);
+    
     % mask = cellfun(@(C), )
-
+    
     for ii = size(c_tbl,1):-1:1
         nan_idx = cell2mat(cellfun(@isnan, c_tbl(ii,:), 'UniformOutput',false));
-
-        % c_tbl(ii,3:end) = num2str(c_tbl(ii,3:end)); 
+        
+        % c_tbl(ii,3:end) = num2str(c_tbl(ii,3:end));
         c_tbl(ii,nan_idx) = {' '};
     end
-
-    tbl_out = cell2table(c_tbl); 
-
-% write a .csv per animal. 
-writetable(tbl, [subs{iS} '_WM.csv'], 'WriteVariableNames',1)
-tbl_in = readtable([subs{iS} '_WM.csv'])
-
+    
+    tbl_out = cell2table(c_tbl);
+    
+    % write a .csv per animal.
+%     writetable(tbl, [subs{iS} '_WM.csv'], 'WriteVariableNames',1)
+    % tbl_in = readtable([subs{iS} '_WM.csv'])
+    
 end
 
 
