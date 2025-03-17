48d5efcf0fbf54d113d4e9fb73778b693a6ffcaa
Eric Carmichael <ecarmichael@gmail.com>
2025-03-17 16:27:11 -0400

    flash detection

--- Masters/Master_TCF.m
+++ Masters/Master_TCF.m
@@ -398,6 +398,12 @@ xlim([0 6])
 ylim(y_l)
 % title('Context A Re-exposure')
 ylabel('Contextual freezing (%)')
+%% convert data into csv
+
+table_out = table(TFC2_sub', TFC2_geno',TFC2_bar.baseline',TFC2_bar.tone', TFC2_bar.trace', TFC2_bar.ITI', TFC3_bar.baseline',...
+    'VariableNames', {'Subject', 'Genotype', 'TFC2_baseline', 'TFC2_tone', 'TFC2_trace', 'TFC2_ITI','TFC3_baseline'});
+
+writetable(table_out, 'TFC_bar_data.csv')
 
 %% save
 
--- sandboxes/sandbox_Ca_Detect_Flash.m
+++ sandboxes/sandbox_Ca_Detect_Flash.m
@@ -14,16 +14,16 @@ end
 % not helpful
 
 %% try getting the median of only the negative values by removing all positives before getting the median. 
-
+ms = MS_Ca_good_cells(ms); 
 
 temp_detrend = NaN(size(ms.detrendRaw));
-p_mat = []; 
+p_mat = []; keep_idx= [];
 for ii = size(ms.detrendRaw,2):-1:1
     
-    temp_detrend(:,ii) = ms.detrendRaw(:,ii);
+    temp_detrend(:,ii) = diff([ms.detrendRaw(:,ii);  ms.detrendRaw(end,ii)]);
     
     
-    [~, p_idx] = findpeaks(-temp_detrend(:,ii), 'MinPeakHeight', .2, 'MinPeakDistance', 10);
+    [~, p_idx] = findpeaks(-temp_detrend(:,ii), 'MinPeakHeight', .1, 'MinPeakDistance', 10);
     if length(p_idx) > 100
     
         p_mat = [p_mat, p_idx']; 
@@ -38,22 +38,79 @@ for ii = size(ms.detrendRaw,2):-1:1
     
 end
 
+keep_idx = logical(keep_idx);
 %%
 figure(161)
 
+med_detrend = median(temp_detrend(:,keep_idx),2); 
+
 clf
 ax(1) = subplot(2,1,1);
-plot(ms.time, median(temp_detrend(:,keep_idx),2))
+hold on
+plot(ms.time, med_detrend)
+% plot(ms.time(sort(p_mat)), med_detrend(sort(p_mat)), 'x')
+
 
 ax(2) = subplot(2,1,2);
 hold on
 h = histogram(ms.time(p_mat), round(length(ms.time)/5));
-yline(prctile(h.Values, 99.5))
-linkaxes(ax, 'x')
+yline(prctile(h.Values, 99))
 
 % get the indicies of the putative flashes from the histogram. 
 
+bin_centers = h.BinEdges(1:end-1)+h.BinWidth; 
 
-% flash_idx = 
+flash_idx =h.Values > prctile(h.Values, 99);
+flash_t = bin_centers(flash_idx);
+
+flash_enc = flash_t(flash_t < ms.time(ms.timestamps(1)));
+flash_rec = flash_t(flash_t > ms.time(sum(ms.timestamps(1:2))+2));
+
+
+plot(flash_enc, h.Values(flash_t < ms.time(ms.timestamps(1))), 'xb')
+plot(flash_rec, h.Values(flash_t > ms.time(sum(ms.timestamps(1:2))+2)), 'xr')
+
+xline(ms.time(sum(ms.timestamps(1:2))+3))
+linkaxes(ax, 'x')
+
+
 
 %% grab specific frames. 
+% restrict to recall only
+
+ms_rec = MS_restrict(ms, ms.time(sum(ms.timestamps(1:2))+3), flash_t(end));
+
+flash_idx = nearest_idx(flash_rec, ms_rec.time);
+
+flash_vec = zeros(size(ms_rec.time));
+
+flash_vec(flash_idx) = 1;
+flash_vec = logical(flash_vec);
+
+
+
+
+flash_rec = flash_rec -ms_rec.time(1);
+
+ms_rec.time = ms_rec.time - ms_rec.time(1); 
+
+behav_rec_a = MS_align_data(behav_rec, ms_rec);
+
+%%
+% move_idx 
+
+mov_idx = behav_rec_a.speed > 2.5; 
+
+
+figure(910)
+clf
+hold on
+plot(behav_rec.time , behav_rec.position(:,1))
+plot(ms_rec.time , ms_rec.denoise(:,1))
+
+MS_event_rate_map(flash_vec(mov_idx) , ms_rec.time(mov_idx), behav_rec_a.position(mov_idx,:), 1, 1);
+
+
+
+
+
