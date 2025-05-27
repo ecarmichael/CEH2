ef55907177498cffe90c0433ea6df9969387b083
ecarmichael <ecarmichael@gmail.com>
2025-05-26 21:14:20 -0400

    CE align

--- CE/CE_sandbox_nlx_mini_align.m
+++ CE/CE_sandbox_nlx_mini_align.m
@@ -1,8 +1,9 @@
-function [csc_out, ms_out]  = CE_sandbox_load_NLX(nlx_dir, CA, evt_idx)
+function [csc_out, ms_out]  = CE_sandbox_load_NLX(nlx_dir, CA,chan_id evt_idx)
 
 
 %% init
-if nargin < 3
+if nargin < 4
+
     evt_idx = []; 
     fprintf('<strong>%s</strong>: no ''evt_idx'' specified using closest match', mfilename)
 end
@@ -15,6 +16,8 @@ if isstring(CA)
 elseif isstruct(CA)
     CA_type = 'ms';
         fprintf('<strong>%s</strong>: ''CA'' input is a struct aligning to pre and post', mfilename)
+    ms_pre = CA.pre.ms; 
+    ms_post = CA.post.ms; 
 
 end
 
@@ -27,7 +30,7 @@ end
 cd(nlx_dir); 
 
 cfg_csc = [];
-cfg_csc.fc = {'CSC1.ncs'}; 
+cfg_csc.fc = {chan_id}; 
 csc = MS_LoadCSC(cfg_csc);
 
 
@@ -43,6 +46,94 @@ csc = MS_LoadCSC(cfg_csc);
 
 
 evts = LoadEvents([]);
+
+% split into the two longest recording blocks
+
+rec_len = []; 
+start_idx = find(contains(evts.label, 'Starting Recording'));
+
+for ii = length(evts.t{start_idx}):-1:1
+
+    rec_len(ii) = evts.t{start_idx+1}(ii) - evts.t{start_idx}(ii); 
+
+end
+
+if length(rec_len) > 2
+    error('More than 2 recording blocks (%f), fix this code if true', length(rec_len)); 
+    % [len, idx] = sort(rec_len); 
+end
+
+pre_evts = restrict(evts,evts.t{start_idx}(1), evts.t{start_idx+1}(1)); 
+post_evts = restrict(evts,evts.t{start_idx}(2), evts.t{start_idx+1}(2)); 
+
+rm_idx = contains(pre_evts.label, 'St');
+pre_evts.t(rm_idx) = [];
+pre_evts.label(rm_idx) = [];
+
+rm_idx = contains(post_evts.label, 'St');
+post_evts.t(rm_idx) = [];
+post_evts.label(rm_idx) = [];
+%% compare the number of timestamps between NLX and CA
+
+if ~isempty(evt_idx)
+    % ms_pre
+    csc_out.pre = []; 
+    csc_out.pre.evts = pre_evts;
+    csc_out.pre.evts.t =[];
+    csc_out.pre.evts.t{1} =sort(unique([pre_evts.t{3}; pre_evts.t{4}]));
+    csc_out.pre.evts.label = [];
+    csc_out.pre.evts.label = 'ms_ttl';
+    % restrict the csc to the first and last TTl
+    csc_out.pre.csc = restrict(csc,csc_out.pre.evts.t{1}(1), csc_out.pre.evts.t{1}(end));   
+    %csc_out.pre.csc.tvec = csc_out.pre.csc.tvec - csc_out.pre.csc.tvec(1); 
+    %csc_out.pre.evts.t{1} = csc_out.pre.evts.t{1} - csc_out.pre.evts.t{1}(1)-evts.t{start_idx}(1); 
+
+
+
+    csc_out.post = []; 
+    csc_out.post.evts = post_evts;
+    csc_out.post.evts.t =[];
+    csc_out.post.evts.t{1} =sort(unique([post_evts.t{3}; post_evts.t{4}]));
+    csc_out.post.evts.label = [];
+    csc_out.post.evts.label = 'ms_ttl';
+    % restrict the csc to the first and last TTl
+    csc_out.post.csc = restrict(csc,csc_out.post.evts.t{1}(1), csc_out.post.evts.t{1}(end));  
+
+  fprintf('<strong>%s</strong>: NLX = %.0f  | MS = %.0f (<strong>%.0f</strong>)  time diff: %.2f\n',...
+        mfilename, length(csc_out.pre.evts.t{1}), length(ms_pre.time),length(csc_out.pre.evts.t{1})-length(ms_pre.time),...
+        (csc_out.pre.evts.t{1}(end) - csc_out.pre.evts.t{1}(1)) - 
+
+
+
+
+else
+    
+    fprintf('PRE\n')
+ for ii = 1:size(pre_evts.t,2)
+    fprintf('<strong>NLX evt %d</strong>: NLX = %.0f  | MS = %.0f (<strong>%.0f</strong>) \n',ii, length(pre_evts.t{ii}), length(ms_pre.time),length(pre_evts.t{ii})-length(ms_pre.time))
+ end
+
+    % make a matrix of all of the on and off times
+    n_ttl = []; 
+    for ii = size(pre_evts.t,2):-1:1
+            for jj = size(pre_evts.t,2):-1:1
+                n_ttl(ii, jj) = length(sort(unique([pre_evts.t{ii};pre_evts.t{jj}])) );
+
+            end
+    end
+    n_ttl = n_ttl - length(ms_pre.time)
+
+
+
+
+
+end
+
+
+
+
+
+%%
 
 rm_idx = contains(evts.label, 'St');
 evts.t(rm_idx) = [];
